05659227d4f0029f3b11b9c9908e84b1910301d8
Akshat01g <ag9092453@gmail.com>
2026-08-23 19:59:12 +0530

    Time: 63 ms (92.68%) | Memory: 252.6 MB (41.48%) - LeetSync

--- 4383-find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.cpp
+++ 4383-find-all-numbers-disappeared-in-an-array-ii/find-all-numbers-disappeared-in-an-array-ii.cpp
@@ -3,54 +3,27 @@ public:
     vector<vector<int>> findDisappearedNumbers(vector<int>& a, int lower, int upper) {
         int n=a.size();
         vector<vector<int>>ans;
-        if(n==0){
-            ans.push_back({lower,upper});
-            return ans;
-        }
-        sort(a.begin(),a.end());
-        
-        if(a[n-1]<lower){
-           ans.push_back({lower,upper});
-           return ans;
-        }
-        int i=0;
-        while(i<n && a[i]<lower){
-            i++;
-        }
-        if(i==n){
-            ans.push_back({lower,upper});
-           return ans;
-        }
-        if(a[i]>lower){
-            ans.push_back({lower,min(a[i]-1,upper)});
-        }
-        
-        // if(i+1<n && a[i]==lower ){
-        //     ans.push_back({lower+1,a[i+1]-1});
-        // }
-        // else if(i+1==n && a[i]==lower){
-        //     ans.push_back({lower+1,upper});
-        //     return ans;
-        // }
-        // else if(a[i]>lower){
-        //     ans.push_back({lower,a[i]-1});
-        // }
-        int prev=a[i];
-        if(i==0){
-            i++;
-        }
-        
-        for(;i<n;i++){
-            if(a[i]>upper){
-                break;
+        vector<int>temp;
+        temp.push_back(lower-1);
+        for(int i=0;i<n;i++){
+            if(a[i]>lower && a[i]<upper){
+                temp.push_back(a[i]);
             }
-            if(a[i]-prev>1){
-                ans.push_back({prev+1,a[i]-1});
+            else if(a[i]==lower || a[i]==upper){
+                temp.push_back(a[i]);
             }
-            prev=a[i];
         }
-        if(upper>prev){
-            ans.push_back({prev+1,upper});
+        temp.push_back(upper+1);
+        sort(temp.begin(),temp.end());
+        temp.erase(unique(temp.begin(),temp.end()),temp.end());
+        for(int i=1;i<temp.size();i++){
+            int l=temp[i-1]+1;
+            int r=temp[i]-1;
+            if(l<=r){
+                ans.push_back({l,r});
+            }
+
+
         }
         return ans;
     }
